86ab7fcda8d115472c625369811dab5497e25e5c
Pranjal Joshi <pranjaljoshi@fico.com>
2013-03-04 17:19:52 +0530

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PranjalApp::Application.config.secret_token = '035dd7ddf68c0c186f04d9ea8785a73a0551e5fa2a66181692e83e7ac73cb0a2c5df384a3c5b790502ca1a7724fc6fc261781ede0eab7baf39bca587b92072c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PranjalApp::Application.config.session_store :cookie_store, key: '_Pranjal_App_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PranjalApp::Application.config.session_store :active_record_store
